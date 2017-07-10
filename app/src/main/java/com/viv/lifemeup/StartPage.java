93941603316885444786e4ade5c87539f0738ba2
vling <vling@hyperwallet.com>
2017-07-09 20:19:48 -0700

    life me up backbone 1

---
+++ app/src/main/java/com/viv/lifemeup/StartPage.java
@@ -0,0 +1,19 @@
+package com.viv.lifemeup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class StartPage extends AppCompatActivity {
+
+    private EditText userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+
+}
